45a1025338df4dfe3bc88fd28afbc47d981369e6
Blerim Ceka <blerim.ceka@i-solve.info>
2021-12-05 19:39:45 +0100

    Building User interface

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,14 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  
-  baseUrl = 'https://localhost:44347/api/';
+
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
@@ -34,7 +35,7 @@ export class AccountService {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
-        return user; 
+        return user;
       })
     )
   }
@@ -47,5 +48,5 @@ export class AccountService {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
-  
+
 }
